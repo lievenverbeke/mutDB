86a628b852c7fce0da4019b64385420009f3f37f
Lieven Verbeke <lieven.verbeke@intec.ugent.be>
2015-06-25 13:23:28 +0200

    coding regions of genes annotations

--- MutationDB/MDB.py
+++ MutationDB/MDB.py
@@ -8,30 +8,107 @@ class MDB:
         self.databasePath = databasePath
         self.dbMS = None
         self.dbSM = None
+        self.dbAnnotation = None
         self.openDatabases()
 
+    def clearAnnotationDB(self):
+        logging.info('clearing annotation database')
+        if self.dbAnnotation is not None:
+            self.dbAnnotation.close()
+        plyvel.destroy_db(self.databasePath + '/mdbAnnotation')
+        self.dbAnnotation = plyvel.DB(self.databasePath + '/mdbAnnotation', create_if_missing=True)
 
-    def findMutationsForSample(self,sampleID):
+    def importCodingAreaOfGenes(self, fileName, idColumn=0, chromosomeColumn=1, startColumn=2, endColumn=3,
+                                skipFirst=True):
+        logging.info('reading coding annotation data from %s', fileName)
+        index = 0
+
+        with open(fileName) as iFile:
+            if skipFirst:
+                iFile.readline()
+            for line in iFile:
+                index += 1
+
+                parts = line.rstrip('\n').split('\t')
+                geneID = MDB.removeDotFromGeneName(parts[idColumn])
+                chromosome = parts[chromosomeColumn]
+                startOfGene = int(parts[startColumn])
+                endOfGene = int(parts[endColumn])
+                mutationsInThisGene = self.findMutationsInRegion(chromosome, startOfGene, endOfGene)
+
+                if mutationsInThisGene is not None:
+                    key = MDB.geneIDToCodingKey(geneID)
+                    value = pickle.dumps(mutationsInThisGene)
+                    self.dbAnnotation.put(key.encode('utf-8'), value)
+
+                if index % 1000 == 0:
+                    logging.info('processed line {:d}'.format(index))
+
+
+    @staticmethod
+    def removeDotFromGeneName(geneName):
+        posOfDot = geneName.find('.')
+        if posOfDot > 0:
+            return geneName[0:posOfDot]
+        else:
+            return geneName
+
+    def findMutationsForSample(self, sampleID):
         if self.dbSM is None:
             logging.info('sample database is not open')
 
-        key=MDB.sampleToString(sampleID).encode('utf-8')
-        value=self.dbSM.get(key)
-        print(key.decode('utf-8'),', '.join(pickle.loads(value)))
+        key = MDB.sampleToString(sampleID).encode('utf-8')
+        value = self.dbSM.get(key)
+        result = pickle.loads(value)
+        return result
 
-    def findSamplesWithMutationInRegion(self,chromosome,fromPos,toPos):
+    def findMutationsInCodingAreaOfGene(self, geneID):
+        if self.dbAnnotation is None:
+            logging.info('annotation database is not open')
+
+        key = MDB.geneIDToCodingKey(geneID).encode('utf-8')
+        value = self.dbAnnotation.get(key)
+        result = pickle.loads(value)
+        return result
+
+    def findSamplesWithMutation(self, chromosome, pos):
+        if self.dbMS is None:
+            logging.info('sample database is not open')
+
+        posS = '{:d}'.format(pos)
+
+        key = MDB.positionToString(chromosome, posS).encode('utf-8')
+        value = self.dbMS.get(key)
+        result = pickle.loads(value)
+        return result
+
+    def findSamplesWithMutationInRegion(self, chromosome, fromPos, toPos):
         if self.dbMS is None:
             logging.info('mutation database is not open')
 
-        fromPosS='{:d}'.format(fromPos)
-        toPosS='{:d}'.format(toPos)
+        fromPosS = '{:d}'.format(fromPos - 1)
+        toPosS = '{:d}'.format(toPos + 1)
 
-        fromKey=MDB.positionToString(chromosome,fromPosS).encode('utf-8')
-        toKey=MDB.positionToString(chromosome,toPosS).encode('utf-8')
+        fromKey = MDB.positionToString(chromosome, fromPosS).encode('utf-8')
+        toKey = MDB.positionToString(chromosome, toPosS).encode('utf-8')
+        result = dict()
         for key, value in self.dbMS.iterator(start=fromKey, stop=toKey):
-            print(key.decode('utf-8'),', '.join(pickle.loads(value)))
+            result[key.decode('utf-8')] = pickle.loads(value)
+        return result
 
-    def importFile(self, fileName):
+    def findMutationsInRegion(self, chromosome, fromPos, toPos):
+        if self.dbMS is None:
+            logging.info('mutation database is not open')
+
+        fromPosS = '{:d}'.format(fromPos - 1)
+        toPosS = '{:d}'.format(toPos + 1)
+
+        fromKey = MDB.positionToString(chromosome, fromPosS).encode('utf-8')
+        toKey = MDB.positionToString(chromosome, toPosS).encode('utf-8')
+        result = list(self.dbMS.iterator(start=fromKey, stop=toKey, include_value=False))
+        return result
+
+    def importMutationFile(self, fileName):
         logging.info('reading data from %s', fileName)
         index = 0
         samplesToAdd = dict()
@@ -65,11 +142,10 @@ class MDB:
         MDB.appendMapToKeyList(self.dbSM, mutationsToAdd)
         MDB.appendMapToKeyList(self.dbMS, samplesToAdd)
 
-
     @staticmethod
     def appendMapToKeyList(db, mapToAppend):
         for key in mapToAppend:
-            value=mapToAppend[key]
+            value = mapToAppend[key]
             MDB.appendListToKeyList(db, key, value)
         return
 
@@ -83,7 +159,6 @@ class MDB:
         db.put(key, pickle.dumps(listToAppend))
         return
 
-
     @staticmethod
     def appendValueToKeyList(db, key, valueToAppend):
         key = key.encode('utf-8')
@@ -96,31 +171,36 @@ class MDB:
         db.put(key, pickle.dumps(listToAppendTo))
         return
 
+    @staticmethod
+    def geneIDToCodingKey(geneID):
+        return 'coding:{:s}'.format(MDB.removeDotFromGeneName(geneID))
+
+    @staticmethod
+    def geneIDToPromotorKey(geneID):
+        return 'promotor:{:s}'.format(MDB.removeDotFromGeneName(geneID))
 
     @staticmethod
     def sampleToString(sampleID):
         return 'sampleID:{:s}'.format(sampleID)
-
 
     @staticmethod
     def positionToString(chromosome, position):
         key = ('chrom:{:s}:pos:{:0>8s}'.format(chromosome, position))
         return key
 
-
     def clear(self):
         logging.info('clearing all data')
         self.closeDatabases()
         plyvel.destroy_db(self.databasePath + '/mdbSM')
         plyvel.destroy_db(self.databasePath + '/mdbMS')
+        plyvel.destroy_db(self.databasePath + '/mdbAnnotation')
         self.openDatabases()
-
 
     def openDatabases(self):
         logging.info('opening databases in folder ' + self.databasePath)
         self.dbMS = plyvel.DB(self.databasePath + '/mdbSM', create_if_missing=True)
         self.dbSM = plyvel.DB(self.databasePath + '/mdbMS', create_if_missing=True)
-
+        self.dbAnnotation = plyvel.DB(self.databasePath + '/mdbAnnotation', create_if_missing=True)
 
     def closeDatabases(self):
         logging.info('closing databases')
@@ -128,14 +208,14 @@ class MDB:
             self.dbMS.close()
         if self.dbSM is not None:
             self.dbSM.close()
-
+        if self.dbAnnotation is not None:
+            self.dbAnnotation.close()
 
     def printMutationDB(self):
         for key, value in self.dbMS:
             keyS = key.decode("utf-8")
             valueS = ' '.join(pickle.loads(value))
             print('ḱey = {:s}, value = {:s}'.format(keyS, valueS))
-
 
     def printSampleDB(self):
         for key, value in self.dbSM:
--- importRunner.py
+++ importRunner.py
@@ -1,21 +1,30 @@
 from MutationDB import MDB
-
 import logging
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.DEBUG)
 
-
-# fileName='/home/share/data/annotated_mafs/Sanger.PCAWG_train2.lite.snv_mnv.maf'
 # fileName = '/home/share/data/annotated_mafs/small_lite_snv_mnv.txt'
-tertFileName = '/home/share/data/annotated_mafs/Santa_Cruz_Pilot_2015_05_04.tert.maflite'
+
+# mutationFileName='/home/share/data/annotated_mafs/Sanger.PCAWG_train2.lite.snv_mnv.maf'
+# tertFileName = '/home/share/data/annotated_mafs/Santa_Cruz_Pilot_2015_05_04.tert.maflite'
+codingAnnotationFileName='/home/share/data/IDConversion.txt'
+
 dbFolder='/home/share/data/annotated_mafs/'
 
 mutationDB=MDB(dbFolder)
-#mutationDB.findSamplesWithMutationInRegion('1',50000,100000)
-#mutationDB.findMutationsForSample('f82d213f-bc99-5b1d-e040-11ac0c486880')
-# mutationDB.clear()
-mutationDB.importFile(tertFileName)
+
+# mutationDB.importMutationFile(mutationFileName)
+# mutationDB.importMutationFile(tertFileName)
 # mutationDB.printMutationDB()
 # mutationDB.printSampleDB()
+
+mutationDB.clearAnnotationDB()
+mutationDB.importCodingAreaOfGenes(codingAnnotationFileName,idColumn=0,chromosomeColumn=1,startColumn=2,endColumn=3,skipFirst=True)
+
+# print(mutationDB.findSamplesWithMutationInRegion('5',1295000,1296000))
+# print(mutationDB.findSamplesWithMutation('5',1295228))
+# print(mutationDB.findMutationsForSample('add0b0f6-b4a8-4b73-b634-c3600dc567d5'))
+print(mutationDB.findMutationsInCodingAreaOfGene('ENSG00000141510'))
+
 
 mutationDB.closeDatabases()
