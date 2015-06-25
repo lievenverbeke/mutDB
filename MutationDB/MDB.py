f1f519437beee4fa3b565abd8a35b86cf9e57bd4
Lieven Verbeke <lieven.verbeke@intec.ugent.be>
2015-06-25 10:01:17 +0200

    mutation and sample queries

--- MutationDB/MDB.py
+++ MutationDB/MDB.py
@@ -10,11 +10,33 @@ class MDB:
         self.dbSM = None
         self.openDatabases()
 
+
+    def findMutationsForSample(self,sampleID):
+        if self.dbSM is None:
+            logging.info('sample database is not open')
+
+        key=MDB.sampleToString(sampleID).encode('utf-8')
+        value=self.dbSM.get(key)
+        print(key.decode('utf-8'),', '.join(pickle.loads(value)))
+
+    def findSamplesWithMutationInRegion(self,chromosome,fromPos,toPos):
+        if self.dbMS is None:
+            logging.info('mutation database is not open')
+
+        fromPosS='{:d}'.format(fromPos)
+        toPosS='{:d}'.format(toPos)
+
+        fromKey=MDB.positionToString(chromosome,fromPosS).encode('utf-8')
+        toKey=MDB.positionToString(chromosome,toPosS).encode('utf-8')
+        for key, value in self.dbMS.iterator(start=fromKey, stop=toKey):
+            print(key.decode('utf-8'),', '.join(pickle.loads(value)))
+
     def importFile(self, fileName):
         logging.info('reading data from %s', fileName)
         index = 0
-        samplesToAdd = list()
-        previousKey = ''
+        samplesToAdd = dict()
+        mutationsToAdd = dict()
+        batchSize = 1000000
 
         with open(fileName) as iFile:
             for line in iFile:
@@ -22,40 +44,69 @@ class MDB:
 
                 parts = line.rstrip('\n').split('\t')
 
-                key = MDB.positionToString(parts[0], parts[1])
-                value = MDB.sampleToString(parts[5])
+                positionS = MDB.positionToString(parts[0], parts[1])
+                sampleS = MDB.sampleToString(parts[5])
 
-                if index > 1 and previousKey != key:
-                    MDB.addListToKey(self.dbMS, previousKey, samplesToAdd)
+                if positionS not in samplesToAdd:
+                    samplesToAdd[positionS] = list()
+                samplesToAdd[positionS].append(sampleS)
+
+                if sampleS not in mutationsToAdd:
+                    mutationsToAdd[sampleS] = list()
+                mutationsToAdd[sampleS].append(positionS)
+
+                if index % batchSize == 0:
+                    MDB.appendMapToKeyList(self.dbSM, mutationsToAdd)
+                    MDB.appendMapToKeyList(self.dbMS, samplesToAdd)
+                    mutationsToAdd.clear()
                     samplesToAdd.clear()
-
-                samplesToAdd.append(value)
-                previousKey = key
-
-                if index % 10000 == 0:
                     logging.info("{:<10d} lines processed, chromosome {:s}".format(index, parts[0]))
 
-        MDB.addListToKey(self.dbMS, previousKey, samplesToAdd)
+        MDB.appendMapToKeyList(self.dbSM, mutationsToAdd)
+        MDB.appendMapToKeyList(self.dbMS, samplesToAdd)
+
+
+    @staticmethod
+    def appendMapToKeyList(db, mapToAppend):
+        for key in mapToAppend:
+            value=mapToAppend[key]
+            MDB.appendListToKeyList(db, key, value)
         return
 
     @staticmethod
-    def addListToKey(db, key, listToAdd):
+    def appendListToKeyList(db, key, listToAppend):
         key = key.encode('utf-8')
         value = db.get(key)
         if value is not None:
-            listToAdd = listToAdd + pickle.loads(value)
+            listToAppend = listToAppend + pickle.loads(value)
 
-        db.put(key, pickle.dumps(listToAdd))
+        db.put(key, pickle.dumps(listToAppend))
         return
+
+
+    @staticmethod
+    def appendValueToKeyList(db, key, valueToAppend):
+        key = key.encode('utf-8')
+        value = db.get(key)
+        if value is not None:
+            listToAppendTo = pickle.loads(value)
+        else:
+            listToAppendTo = list()
+        listToAppendTo.append(valueToAppend)
+        db.put(key, pickle.dumps(listToAppendTo))
+        return
+
 
     @staticmethod
     def sampleToString(sampleID):
         return 'sampleID:{:s}'.format(sampleID)
 
+
     @staticmethod
     def positionToString(chromosome, position):
         key = ('chrom:{:s}:pos:{:0>8s}'.format(chromosome, position))
         return key
+
 
     def clear(self):
         logging.info('clearing all data')
@@ -64,10 +115,12 @@ class MDB:
         plyvel.destroy_db(self.databasePath + '/mdbMS')
         self.openDatabases()
 
+
     def openDatabases(self):
-        logging.info('opening databases in folder '+ self.databasePath)
+        logging.info('opening databases in folder ' + self.databasePath)
         self.dbMS = plyvel.DB(self.databasePath + '/mdbSM', create_if_missing=True)
         self.dbSM = plyvel.DB(self.databasePath + '/mdbMS', create_if_missing=True)
+
 
     def closeDatabases(self):
         logging.info('closing databases')
@@ -76,11 +129,13 @@ class MDB:
         if self.dbSM is not None:
             self.dbSM.close()
 
+
     def printMutationDB(self):
         for key, value in self.dbMS:
             keyS = key.decode("utf-8")
             valueS = ' '.join(pickle.loads(value))
             print('ḱey = {:s}, value = {:s}'.format(keyS, valueS))
+
 
     def printSampleDB(self):
         for key, value in self.dbSM:
--- importRunner.py
+++ importRunner.py
@@ -1,18 +1,20 @@
 from MutationDB import MDB
 
 import logging
-import pickle
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.DEBUG)
 
 
-# fileName='/home/share/data/annotated_mafs/Sanger.PCAWG_train2.lite.snv_mnv.maf'
-fileName = '/home/share/data/annotated_mafs/small_lite_snv_mnv.txt'
+fileName='/home/share/data/annotated_mafs/Sanger.PCAWG_train2.lite.snv_mnv.maf'
+# fileName = '/home/share/data/annotated_mafs/small_lite_snv_mnv.txt'
 dbFolder='/home/share/data/annotated_mafs/'
 
 mutationDB=MDB(dbFolder)
-mutationDB.clear()
-mutationDB.importFile(fileName)
-mutationDB.printMutationDB()
+#mutationDB.findSamplesWithMutationInRegion('1',50000,100000)
+mutationDB.findMutationsForSample('f82d213f-bc99-5b1d-e040-11ac0c486880')
+# mutationDB.clear()
+# mutationDB.importFile(fileName)
+# mutationDB.printMutationDB()
+# mutationDB.printSampleDB()
 
 mutationDB.closeDatabases()
